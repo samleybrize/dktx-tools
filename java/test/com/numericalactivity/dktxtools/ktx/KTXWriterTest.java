38759d5673a165deef236a6eae86214ae5070f8a
Stephen Berquet <stephen.berquet@gmail.com>
2012-03-19 14:12:54 +0000

    [ENH] dktx-tools : junit : chargement unique de tous les mipmaps de la texture non compressée

--- java/test/com/numericalactivity/dktxtools/ktx/KTXWriterTest.java
+++ java/test/com/numericalactivity/dktxtools/ktx/KTXWriterTest.java
@@ -16,23 +16,44 @@ import com.numericalactivity.dktxtools.utils.BufferUtils;
 
 public class KTXWriterTest {
 
+    protected static final byte FILE_NUMBER_OF_MIPMAPS              = 9;
+    protected static final String FILE_UNCOMPRESSED_TEXTURE_BASE    = "./testRes/uncompressed/texture";
+    protected static final String FILE_UNCOMPRESSED_NO_MIPMAP       = "./testRes/ktx/uncompressed_no_mipmap.ktx";
+    protected static final String FILE_UNCOMPRESSED_NO_MIPMAP_GEN   = "./testRes/gen/uncompressed_no_mipmap.ktx";
+
+    protected ByteBuffer[] _uncompressedTextureBuffer;
+
+    public KTXWriterTest() {
+        try {
+            _uncompressedTextureBuffer = new ByteBuffer[FILE_NUMBER_OF_MIPMAPS];
+            BufferedImage img;
+            short width;
+            short height;
+            int[] rgbArray;
+
+            for (byte i = 0; i < 9; i++) {
+                img                             = ImageIO.read(new File(FILE_UNCOMPRESSED_TEXTURE_BASE + String.valueOf(i) + ".jpg"));
+                width                           = (short) img.getWidth();
+                height                          = (short) img.getHeight();
+                rgbArray                        = new int[img.getWidth() * img.getHeight()];
+                img.getRGB(0, 0, width, height, rgbArray, width * height - width, -width);
+                _uncompressedTextureBuffer[i]   = BufferUtils.getPixelBuffer(rgbArray, TextureFormat.GL_RGBA);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e.getMessage());
+        }
+    }
+
     @Test
     public void testWriteUncompressedNoMipmap() {
         try {
-            BufferedImage img       = ImageIO.read(new File("./testRes/texture.jpg"));
-            short width             = (short) img.getWidth();
-            short height            = (short) img.getHeight();
-            int[] rgbArray          = new int[img.getWidth() * img.getHeight()];
-            img.getRGB(0, 0, width, height, rgbArray, width * height - width, -width);
-            ByteBuffer buffer       = BufferUtils.getPixelBuffer(rgbArray, TextureFormat.GL_RGBA);
-            FileOutputStream out    = new FileOutputStream("./testRes/gen/uncompressed_no_mipmap.ktx");
-
+            FileOutputStream out    = new FileOutputStream(FILE_UNCOMPRESSED_NO_MIPMAP_GEN);
             KTXWriter writer        = new KTXWriter(false, false, 256, 256);
             writer.setUncompressedFormat(TextureFormat.GL_RGBA);
-            writer.getTextureData().set(0, buffer);
+            writer.getTextureData().set(0, _uncompressedTextureBuffer[0]);
             writer.write(out);
         } catch (Exception e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
             fail(e.getMessage());
         }
